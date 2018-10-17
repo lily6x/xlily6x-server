0d323e5d4c1f5083839c1fd1bd5b739b71b3a2ed
Lily6x <xiaowenlong2015@icloud.com>
2018-10-17 15:27:19 +0800

    配置调整

--- src/test/java/com/xlily6x/server/Xlily6xServerApplicationTests.java
+++ src/test/java/com/xlily6x/server/Xlily6xServerApplicationTests.java
@@ -1,25 +1,25 @@
 package com.xlily6x.server;
 
 import com.xlily6x.server.common.HttpClient;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class Xlily6xServerApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
-
-    @Test
-    public void test030(){
-        String url = "http://ip.taobao.com/service/getIpInfo.php";
-        String result = HttpClient.sendGet(url,"ip=202.53.138.30");
-        System.out.println(result);
-    }
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//    @Test
+//    public void test030(){
+//        String url = "http://ip.taobao.com/service/getIpInfo.php";
+//        String result = HttpClient.sendGet(url,"ip=202.53.138.30");
+//        System.out.println(result);
+//    }
 
     //    @Test
 //    public void test(){
